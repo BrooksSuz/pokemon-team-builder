39d43af26ca0f6bdb5a5fb390de110b2d76faae2
Brooks Susor <susorbrooks@gmail.com>
2022-12-07 12:55:08 -0500

    Fix children to flex properly

--- src/components/Pokemon/PokemonTeam.js
+++ src/components/Pokemon/PokemonTeam.js
@@ -4,9 +4,12 @@ const PokemonTeam = () => {
       className='pokemon-team'
       data-testid='pokemonTeam'
     >
-      <div>
-        This will contain a party of six pokemon.
-      </div>
+      <div className="party-1">poke-1</div>
+      <div className="party-2">poke-2</div>
+      <div className="party-3">poke-3</div>
+      <div className="party-4">poke-4</div>
+      <div className="party-5">poke-5</div>
+      <div className="party-6">poke-6</div>
     </div>
   );
 };
