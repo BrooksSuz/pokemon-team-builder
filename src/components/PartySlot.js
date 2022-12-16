ca6a384ec39aeabcb336486d3ffb2f7917d0faf1
Brooks Susor <susorbrooks@gmail.com>
2022-12-16 12:10:09 -0500

    Create hide/show delete button functionality

--- src/components/PartySlot.js
+++ src/components/PartySlot.js
@@ -1,8 +1,9 @@
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 
 const PartySlot = (props) => {
   const { party, setParty, i } = props;
   const divSlot = useRef();
+  const btnDelete = useRef();
 
   const handleClick = () => {
     const copyParty = Object.assign({}, party);
@@ -13,8 +14,15 @@ const PartySlot = (props) => {
     }
 
     setParty(copyParty);
-    
   };
+
+  useEffect(() => {
+    if (divSlot.current.childNodes[0].textContent) {
+      btnDelete.current.style.visibility = '';
+    } else if (!divSlot.current.childNodes[0].textContent) {
+      btnDelete.current.style.visibility = 'hidden';
+    }
+  }, [party]);
 
   return (
     <div
@@ -22,7 +30,12 @@ const PartySlot = (props) => {
       ref={divSlot}
     >
       <span>{party[`poke-${i + 1}`]}</span>
-      <button onClick={handleClick}>Delete Pokemon</button>
+      <button
+        onClick={handleClick}
+        style={{ visibility: 'hidden' }}
+        ref={btnDelete}
+      >Delete Pokemon
+      </button>
     </div>
   );
 };
