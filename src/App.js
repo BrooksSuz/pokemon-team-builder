b23a2ba7907882cdeec02acbe3771be94e5a968b
Brooks Susor <susorbrooks@gmail.com>
2022-12-16 18:31:45 -0500

    Create onClickDeleteParty functionality

--- src/App.js
+++ src/App.js
@@ -37,111 +37,120 @@ const App = () => {
     setParty(copyParty);
   };
 
+  const onClickDeleteParty = () => {
+    setParty(Array.from({ length: 6 }, () => {
+      return { pokeName: '', pokeSprite: '' };
+    }));
+  };
+
   useEffect(() => {
     getPaldeaPokedex().then(res => setPokedex(res));
   }, []);
 
   return (
     <>
-    <header data-testid='header'>
-      <button>Delete Entire Party</button>
-      <h1>Pokemon Scarlet & Violet Team Builder</h1>
-      <p>This is a team builder for the newest Pokemon games.</p>
-    </header>
+      <header data-testid='header'>
+        <h1>Pokemon Scarlet & Violet Team Builder</h1>
+        <p>This is a team builder for the newest Pokemon games.</p>
+        <button
+          style={{ alignSelf: 'flex-start' }}
+          onClick={onClickDeleteParty}
+          >Delete Entire Party</button>
+      </header>
 
-    {/* Start parent of pokemon-team & pokemon-list */}
-    <div
-      className='pokemon-container'
-      data-testid='pokemonContainer'
-    >
-
-      {/* Start pokemon-team */}
+      {/* Start parent of pokemon-team & pokemon-list */}
       <div
-        className='pokemon-team'
-        data-testid='pokemonTeam'
+        className='pokemon-container'
+        data-testid='pokemonContainer'
       >
-        {
-          Array.from({ length: 6 }, (_, i) => 
-            <PartySlot
-              party={party}
-              setParty={setParty}
-              i={i}
-              key={i}
-            />
-          )
-        }
-      </div>
-      {/* End pokemon-team */}
 
-      {/* Start pokemon-list */}
-      <div
-      className='pokemon-list'
-      data-testid='pokemonList'
-      >
-        <label> Search Pokedex:
-          <input
-            type='text'
-            name='search-dex'
-            placeholder='Paldea Awaits...'
-            onChange={e => setSearchTerm(e.target.value)}
-          />
-        </label>
-
-        {/* Start entry-container */}
+        {/* Start pokemon-team */}
         <div
-          className='entry-container'
+          className='pokemon-team'
+          data-testid='pokemonTeam'
         >
           {
-            pokedex
-              .filter(pokemon => {
-                return searchTerm === ''
-                  ? pokemon
-                  : pokemon.includes(searchTerm);
-              })
-              .map((pokemon, i) => (
-              <button
-                className='entry'
+            Array.from({ length: 6 }, (_, i) => 
+              <PartySlot
+                party={party}
+                setParty={setParty}
+                i={i}
                 key={i}
-                onClick={e => onClickAddPokemon(e.target.childNodes[2].textContent)}
-              >
-                <span>
-                  {
-                    pokedex.findIndex(entry => {
-                      return pokemon === entry
-                        ? true
-                        : false
-                    }) + 1
-                  }
-                </span>
-                <img
-                  src={`https://img.pokemondb.net/sprites/scarlet-violet/normal/${pokemon}.png`}
-                  alt={`pokedex entry number ${i + 1}`} 
-                  height='64'
-                  width='64'
-                  style={{ marginBottom: '5px' }}
-                />
-                <span>
-                  {
-                    pokemon.includes('-')
-                      ? pokemon.charAt(0).toUpperCase() + pokemon.slice(1).replace('-', ' ')
-                      : pokemon.charAt(0).toUpperCase() + pokemon.slice(1)
-                  }
-                </span>
-              </button>
-            ))
+              />
+            )
           }
         </div>
-        {/* End entry-container */}
+        {/* End pokemon-team */}
+
+        {/* Start pokemon-list */}
+        <div
+        className='pokemon-list'
+        data-testid='pokemonList'
+        >
+          <label> Search Pokedex:
+            <input
+              type='text'
+              name='search-dex'
+              placeholder='Paldea Awaits...'
+              onChange={e => setSearchTerm(e.target.value)}
+            />
+          </label>
+
+          {/* Start entry-container */}
+          <div
+            className='entry-container'
+          >
+            {
+              pokedex
+                .filter(pokemon => {
+                  return searchTerm === ''
+                    ? pokemon
+                    : pokemon.includes(searchTerm);
+                })
+                .map((pokemon, i) => (
+                <button
+                  className='entry'
+                  key={i}
+                  onClick={e => onClickAddPokemon(e.target.childNodes[2].textContent)}
+                >
+                  <span>
+                    {
+                      pokedex.findIndex(entry => {
+                        return pokemon === entry
+                          ? true
+                          : false
+                      }) + 1
+                    }
+                  </span>
+                  <img
+                    src={`https://img.pokemondb.net/sprites/scarlet-violet/normal/${pokemon}.png`}
+                    alt={`pokedex entry number ${i + 1}`} 
+                    height='64'
+                    width='64'
+                    style={{ marginBottom: '5px' }}
+                  />
+                  <span>
+                    {
+                      pokemon.includes('-')
+                        ? pokemon.charAt(0).toUpperCase() + pokemon.slice(1).replace('-', ' ')
+                        : pokemon.charAt(0).toUpperCase() + pokemon.slice(1)
+                    }
+                  </span>
+                </button>
+              ))
+            }
+          </div>
+          {/* End entry-container */}
+
+        </div>
+        {/* End pokemon-list */}
 
       </div>
-      {/* End pokemon-list */}
-
-    </div>
-    {/* End parent of pokemon-team & pokemon-list */}
-      
-    <footer data-testid='footer'>
-      This project was made by me :D
-    </footer>
+      {/* End parent of pokemon-team & pokemon-list */}
+        
+      <footer data-testid='footer'>
+        This project was made by me :D
+      </footer>
     </>
   );
 };
