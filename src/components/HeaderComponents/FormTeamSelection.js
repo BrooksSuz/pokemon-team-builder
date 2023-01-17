538663a713ebcf98fb2dd7b7c873be6591552ea1
Brooks Susor <susorbrooks@gmail.com>
2023-01-17 18:46:17 -0500

    Fix save/update team button

--- src/components/HeaderComponents/FormTeamSelection.js
+++ src/components/HeaderComponents/FormTeamSelection.js
@@ -1,15 +1,19 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { getParties, updateParty } from "../../firebase-functions";
 
 const FormTeamSelection = (props) => {
   const { party, setParty, userSignedIn } = props;
-  const selectTeam = useRef();
+  const [currentParty, setCurrentParty] = useState('');
+  const btn1 = useRef();
+  const btn2 = useRef();
+  const btn3 = useRef();
   const btnSave = useRef();
 
   // Update party for current user
   const updatePaldeaParty = () => {
-    updateParty(party, selectTeam.current.options[selectTeam.current.options.selectedIndex].value)
+    updateParty(party, currentParty)
       .then(alert('Party updated!'));
+    
   };
 
   // On userSignedin update to true, get current user's party 1
@@ -22,22 +26,22 @@ const FormTeamSelection = (props) => {
 
   return (
     <div className='team-selection'>
-      <button onClick={() => getParties().then(res => setParty(res.parties['party-1']))}>
+      <button ref={btn1} onClick={() => getParties().then(res => setParty(res.parties['party-1'])).then(setCurrentParty('party-1'))}>
         Party 1
       </button>
-      <button onClick={() => getParties().then(res => setParty(res.parties['party-2']))}>
+      <button ref={btn2} onClick={() => getParties().then(res => setParty(res.parties['party-2'])).then(setCurrentParty('party-2'))}>
         Party 2
       </button>
-      <button onClick={() => getParties().then(res => setParty(res.parties['party-3']))}>
+      <button ref={btn3} onClick={() => getParties().then(res => setParty(res.parties['party-3'])).then(setCurrentParty('party-3'))}>
         Party 3
       </button>
-    <button
-      type='button'
-      onClick={updatePaldeaParty}
-      ref={btnSave}
-    >
-      Save/Update Team
-    </button>
+      <button
+        type='button'
+        onClick={updatePaldeaParty}
+        ref={btnSave}
+      >
+        Save/Update Team
+      </button>
     </div>
   );
 };
